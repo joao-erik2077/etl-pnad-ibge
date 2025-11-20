ba65a5d69c2eaf3b5cbd4f459a0607d7d093098d
João Erik da Silva Crisóstomo <joaoerik.dev@gmail.com>
2025-11-20 16:59:12 -0300

    clean> clean code for query creation

--- main.py
+++ main.py
@@ -7,26 +7,26 @@ POSTGRES_URL = get_database_url()
 logging.basicConfig(level=logging.INFO, format="%(asctime)s [ETL] %(message)s")
 
 def build_pnad_query() -> str:
-    query = SearchQueryBuilder("basedosdados.br_ibge_pnadc.microdados")
+    query = (
+        SearchQueryBuilder(table_name="basedosdados.br_ibge_pnadc.microdados")
+        .add_column(column_name="ano")
+        .add_column_with_alias(column_name="sigla_uf", alias="uf")
+        .add_column_with_alias(column_name="V2007", alias="sexo")
+        .add_column_with_alias(column_name="V2009", alias="idade")
+        .add_column_with_alias(column_name="VD4019", alias="renda_domiciliar")
+        .add_column_with_alias(column_name="V2001", alias="moradores")
+        .add_column_with_alias(column_name="V1027", alias="peso")
 
-    query.add_column("ano")
-    query.add_column_with_alias("sigla_uf", "uf")
-    query.add_column_with_alias("V2007", "sexo")
-    query.add_column_with_alias("V2009", "idade")
-    query.add_column_with_alias("VD4019", "renda_domiciliar")
-    query.add_column_with_alias("V2001", "moradores")
-    query.add_column_with_alias("V1027", "peso")
+        .column_greater_than_or_equal(column_name="ano", value=2022)
+        
+        .order_by(column_name="ano")
 
-    query.column_greater_than_or_equal("ano", 2022)
-    
-    query.order_by("ano")
-
-    query.add_limit(1000)
-    
+        .add_limit(limit_value=1000)
+    )
     return query.get_query()
 
 def run():
-    build_pnad_query()
+    print(build_pnad_query())
 
 if __name__ == "__main__":
     run()
